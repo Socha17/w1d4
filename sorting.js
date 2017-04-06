82059c07ba0fa88c82f2e7bc61616a80222038aa
Socha17 <mattsocha11@gmail.com>
2017-04-06 19:18:09 +0000

    sorting stuff that took way too long

---
+++ sorting.js
@@ -0,0 +1,45 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+
+var sortingLogic = function(a, b) {
+  if (a.name[0] === b.name[0]) {
+    return a.age - b.age
+  } else {
+    if (a.name[0] < b.name[0]) {
+      return -1;
+    } else {
+      return 1;
+    }
+  }
+}
+
+
+//
+//   students.sort(function(a, b) {
+//
+//   if (a < b) {
+//     return -1;
+//   }
+//   if (a > b) {
+//     return 1;
+//   }
+//
+//   // names must be equal
+//   return 0;
+// });
+//
+//   students.sort(function (a, b) {
+//     return a.value - b.value;
+//   });
+//
+//
+// }
+
+
+
+console.log(students.sort(sortingLogic));
